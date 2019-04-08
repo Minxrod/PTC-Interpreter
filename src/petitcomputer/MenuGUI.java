414b3257c2a77597ca1fd3b56b4058dd46ad01e2
Minxrod <minxrod@gmail.com>
2019-04-08 19:41:12 -0400

    Attempt 2 at adding the files as I see them...

---
+++ src/petitcomputer/MenuGUI.java
@@ -0,0 +1,82 @@
+package petitcomputer;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+
+/**
+ * Display and input handling.
+ * @author minxr
+ */
+public class MenuGUI {
+    final String LOAD_RUN = "Open File";
+        
+    JFrame frame;
+    JFileChooser files;
+    JLabel egg;
+    JPanel total;
+    JButton load;
+    
+    boolean pleaseLoad = false;
+    boolean pleaseRun = false;
+    
+    public MenuGUI(){
+        frame = new JFrame("PTC interpreter[?]");
+
+        total = new JPanel(new GridLayout(2,1));
+        
+        files = new JFileChooser();
+        
+        load = new JButton(LOAD_RUN);
+        load.setActionCommand(LOAD_RUN);
+        load.addActionListener(new ButtonListener());
+        
+        egg = new JLabel("WIP Interpreter/Emulator using Java Swing"); //egg
+        
+        //total.add(file);
+        total.add(load);
+        //total.addKeyListener(new KeyboardListener());
+        //total.setFocusable(true);
+        total.add(egg); //egg
+        
+        frame.add(total);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setAlwaysOnTop(true); //useful for testing
+    }
+    
+    private class ButtonListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String name = e.getActionCommand();
+            
+            if (name.equals(LOAD_RUN)){
+                int c = files.showOpenDialog(frame);
+                
+                if (c == JFileChooser.APPROVE_OPTION){
+                    PetitComGUI ptcgui = new PetitComGUI();
+                    Thread ptc = new Thread(ptcgui);
+                    ptcgui.start();
+                    ptc.start();
+                    
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(MenuGUI.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    //Debug.print(Debug.GUI_FLAG, files.getSelectedFile());
+                    ptcgui.getProcess().setFile(files.getSelectedFile()); //fix later, it works for now
+                    ptcgui.getProcess().pleaseLoad();
+                    ptcgui.getProcess().pleaseRun();
+                    
+
+                    
+                }
+            }
+        }
+    }
+}
